3983d8bbc0a211d7b5bd1572861112c05c08f3c5
Sergei Mikhailov <sergei.michailow@gmail.com>
2024-11-17 21:00:05 +0100

    Added solutions

--- HomeTasks/obj/Debug/net8.0/HomeTasks.AssemblyInfo.cs
+++ HomeTasks/obj/Debug/net8.0/HomeTasks.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HomeTasks")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0767d8d34e46f5565f14795e9600e4af7629c547")]
 [assembly: System.Reflection.AssemblyProductAttribute("HomeTasks")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HomeTasks")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
